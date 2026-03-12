1af7d1f4b10f274456448915d7e478bc7d5d3e9b
buyn <buyn.tex@gmail.com>
2026-03-12 05:24:38 +0200

    tailwind is workig

--- src/ueava/header.cljs
+++ src/ueava/header.cljs
@@ -1,13 +1,26 @@
 (ns ueava.header)
 
+;; (defn header []
+;;   [:header.header
+;;    [:div.container
+;;     [:div.logo "UEAVA"]
+;;     [:nav.menu
+;;      [:a {:href "#"} "Home"]
+;;      [:a {:href "#"} "About"]
+;;      [:a {:href "#"} "Resources"]
+;;      [:a {:href "#"} "Membership"]
+;;      [:a {:href "#"} "Conferences"]]
+;;     [:button.cta "Find A Vet"]]])
+
 (defn header []
-  [:header.header
-   [:div.container
-    [:div.logo "UEAVA"]
-    [:nav.menu
-     [:a {:href "#"} "Home"]
-     [:a {:href "#"} "About"]
-     [:a {:href "#"} "Resources"]
-     [:a {:href "#"} "Membership"]
-     [:a {:href "#"} "Conferences"]]
-    [:button.cta "Find A Vet"]]])
+  [:header {:class "flex items-center justify-between px-10 py-4 bg-white shadow"}
+   [:div {:class "text-2xl font-bold text-green-700"} "UEAVA"]
+
+   [:nav {:class "space-x-6 font-medium"}
+    [:a {:class "hover:text-green-600"} "Home"]
+    [:a {:class "hover:text-green-600"} "About"]
+    [:a {:class "hover:text-green-600"} "Resources"]
+    [:a {:class "hover:text-green-600"} "Membership"]]
+
+   [:button {:class "bg-orange-500 text-white px-4 py-2 rounded"}
+    "Find A Vet"]])
--- src/ueava/main.cljs
+++ src/ueava/main.cljs
@@ -1,12 +1,24 @@
 
 (ns ueava.main)
 
+;; (defn hero []
+;;   [:section.hero
+;;    [:div.hero-content
+;;     [:h1 "Why Do You Need A Reptile And Amphibian Veterinarian?"]
+;;     [:p "A global community dedicated to improving reptile and amphibian care."]
+;;     [:button "Learn More"]]])
+
 (defn hero []
-  [:section.hero
-   [:div.hero-content
-    [:h1 "Why Do You Need A Reptile And Amphibian Veterinarian?"]
-    [:p "A global community dedicated to improving reptile and amphibian care."]
-    [:button "Learn More"]]])
+  [:section {:class "h-screen bg-cover bg-center flex items-center justify-center text-white"}
+   [:div {:class "text-center max-w-3xl"}
+    [:h1 {:class "text-5xl font-bold mb-6"}
+     "Why Do You Need A Reptile Veterinarian?"]
+
+    [:p {:class "text-xl mb-8"}
+     "Global community improving reptile and amphibian medicine"]
+
+    [:button {:class "bg-green-600 px-6 py-3 rounded text-white hover:bg-green-700"}
+     "Learn More"]]])
 
 (defn membership []
   [:section.membership
