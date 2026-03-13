8ac4ae484e86f154f46560c80295befb60317158
buyn <buyn.tex@gmail.com>
2026-03-13 22:59:53 +0200

    add comented patern in hero

--- src/ueava/components/hero.cljs
+++ src/ueava/components/hero.cljs
@@ -3,11 +3,22 @@
 (defn hero []
   [:section {:class "relative h-screen flex items-center justify-center pt-24 text-white"}
 
+   ;; background image
    [:div {:class "absolute inset-0 bg-cover bg-center"
           :style {:background-image "url('img/Hero_Slide_2-1.webp')"}}]
 
+   ;; pattern overlay
+   ;; [:div {:class
+   ;;        "absolute inset-0
+   ;;         bg-[url('/img/ueava-logo.svg')]
+   ;;         bg-repeat
+   ;;         bg-[length:300px]
+   ;;         opacity-[0.1]"}]
+
+   ;; gradient overlay
    [:div {:class "absolute inset-0 bg-gradient-to-r from-black/70 via-black/40 to-black/10"}]
 
+   ;; content
    [:div {:class "relative z-10 text-center max-w-4xl px-6"}
 
     [:h1 {:class "text-4xl md:text-6xl font-bold mb-6 leading-tight"}
