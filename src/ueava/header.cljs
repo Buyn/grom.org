08b46d47e8521fda69a00761d99ad6cca12d9241
buyn <buyn.tex@gmail.com>
2026-03-14 02:11:51 +0200

    add humburger menu effects

--- src/ueava/header.cljs
+++ src/ueava/header.cljs
@@ -6,6 +6,28 @@
 (defonce scrolled? (r/atom false))
 (defonce menu-open? (r/atom false))
 
+(defn icon-menu []
+[:svg {:xmlns "http://www.w3.org/2000/svg"
+       :class "w-8 h-8"
+       :fill "none"
+       :viewBox "0 0 24 24"
+       :stroke "currentColor"}
+ [:path {:stroke-linecap "round"
+         :stroke-linejoin "round"
+         :stroke-width "2"
+         :d "M4 6h16M4 12h16M4 18h16"}]])
+
+(defn icon-close []
+[:svg {:xmlns "http://www.w3.org/2000/svg"
+       :class "w-8 h-8"
+       :fill "none"
+       :viewBox "0 0 24 24"
+       :stroke "currentColor"}
+ [:path {:stroke-linecap "round"
+         :stroke-linejoin "round"
+         :stroke-width "2"
+         :d "M6 18L18 6M6 6l12 12"}]])
+
 
 (defn on-scroll []
   (reset! scrolled? (> (.-scrollY js/window) 50)))
@@ -38,12 +60,6 @@
                 :class "h-12"}]]
 
         ;; navigation
-        ;; [:nav {:class "hidden md:flex space-x-8 font-medium"}
-        ;;  [:a {:href "#" :class "hover:text-green-500"} "Home"]
-        ;;  [:a {:href "#" :class "hover:text-green-500"} "About"]
-        ;;  [:a {:href "#" :class "hover:text-green-500"} "Resources"]
-        ;;  [:a {:href "#" :class "hover:text-green-500"} "Membership"]
-        ;;  [:a {:href "#" :class "hover:text-green-500"} "Conferences"]]
         [:nav {:class "hidden md:flex space-x-8 font-medium"}
          [:a {:href "#" :class "hover:text-ueava-sand"} "Home"]
          [:a {:href "#" :class "hover:text-ueava-sand"} "About"]
@@ -59,29 +75,30 @@
          "Find A Vet"]
 
        [:button
-        {:class "md:hidden text-3xl"
+        {:class "md:hidden text-3xl text-ueava-sand"
           :on-click #(swap! menu-open? not)}
-        "☰"]
+        ;; (if @menu-open?
+        ;;   "✕" "☰")]
+        (if @menu-open?
+          [icon-close]
+          [icon-menu])]
 
-        (when @menu-open?
-          [:div
-            {:class "absolute top-full left-0 w-full bg-white shadow-md md:hidden text-ueava-brown"}
-
-            [:div {:class "flex flex-col p-6 space-y-4"}
+        [:div {:class (str
+                  "absolute top-full left-0 w-full bg-ueava-cream shadow-md md:hidden
+                   text-ueava-brown
+                   transform transition-all duration-500 ease-out "
+                  (if @menu-open?
+                    "opacity-100 translate-y-0"
+                    "opacity-0 -translate-y-4 pointer-events-none"))}
+              [:div {:class "flex flex-col p-6 space-y-4"}
             [:a {:href "#" :class "hover:text-ueava-sand"} "Home"]
             [:a {:href "#" :class "hover:text-ueava-sand"} "About"]
             [:a {:href "#" :class "hover:text-ueava-sand"} "Resources"]
             [:a {:href "#" :class "hover:text-ueava-sand"} "Membership"]
             [:a {:href "#" :class "hover:text-ueava-sand"} "Conferences"]]
-
-            [:button
-              {:class "bg-orange-500 text-white px-4 py-2 rounded
-                        hover:bg-orange-600 
-                        space-x-8 font-medium"}
-              "Find A Vet"]
-           ]
-
-          )
+              [:button
+                {:class "bg-orange-500 text-white px-4 py-2 rounded
+                          hover:bg-orange-600 
+                          space-x-8 font-medium"}
+                "Find A Vet"]]
         ]])}))
-
-;; ])
