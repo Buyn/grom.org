840de75cb21085609be1ce8367b424ac7b4a0b5b
buyn <buyn.tex@gmail.com>
2026-03-17 08:56:30 +0200

    hiro select slide hilight

--- src/ueava/components/hero.cljs
+++ src/ueava/components/hero.cljs
@@ -100,8 +100,9 @@
   [:div
    {:class (str
             "w-16 h-16 bg-cover bg-center cursor-pointer border "
-            (when (= idx @current-slide)
-              "border-white"))
+            (if (= idx @current-slide)
+              "border-white scale-110"
+              "border-transparent opacity-70"))
 
     :style {:background-image (str "url('" img "')")}
 
@@ -112,7 +113,8 @@
 
 (defn mini-logo-navigation []
   [:div
-   {:class "absolute bottom-6 right-6 grid grid-cols-2 gap-2"}
+   {:class "absolute bottom-6 right-6 grid grid-cols-2 gap-2 "}
+
 
    (doall
     (map-indexed nav-item slides))])
@@ -146,41 +148,7 @@
 
         ;; scroll indicator
         [:div {:class "absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce"}
-        ;; [:div {:class "absolute left-1/2 -translate-x-1/2 animate-bounce"}
             [:span {:class "text-white text-2xl"} "↓"]]
 
        [mini-logo-navigation]])}))
 
-;; (ns ueava.components.hero)
-
-;; (defn hero []
-;;   [:section {:class "relative h-screen flex items-center justify-center pt-24 text-white"}
-
-;;    ;; background image
-;;    [:div {:class "absolute inset-0 bg-cover bg-center"
-;;           :style {:background-image "url('img/Hero_Slide_2-1.webp')"}}]
-
-;;    ;; gradient overlay
-;;    [:div {:class "absolute inset-0 bg-gradient-to-r from-black/70 via-black/40 to-black/10"}]
-
-;;    ;; content
-;;    [:div {:class "relative z-10 text-center max-w-4xl px-6"}
-
-;;     [:h1 {:class "text-4xl md:text-6xl font-bold mb-6 leading-tight"}
-;;      "Why Do You Need A Reptile And Amphibian Veterinarian?"]
-
-;;     [:p {:class "text-lg md:text-xl mb-10 text-gray-200"}
-;;      "A global community dedicated to improving the quality of medicine,
-;;       conservation, and care of reptiles and amphibians."]
-
-;;     [:div {:class "flex justify-center gap-4"}
-;;      [:button
-;;       {:class "bg-ueava-brown-600 hover:bg-ueava-brown-700 text-white px-8 py-3 rounded"}
-;;       "Become a Member"]
-
-;;      [:button {:class "border border-white px-8 py-3 rounded hover:bg-white hover:text-black transition"}
-;;       "Find a Vet"]]]
-
-;;    ;; scroll indicator
-;;    [:div {:class "absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce"}
-;;     [:span {:class "text-white text-2xl"} "↓"]]])
