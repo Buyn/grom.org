c81ff5389c44b798e37ade9ae2fd72f4cbce80a2
buyn <buyn.tex@gmail.com>
2026-03-17 12:27:24 +0200

    remove commented code

--- src/ueava/components/hero.cljs
+++ src/ueava/components/hero.cljs
@@ -5,7 +5,6 @@
 ;; ----------------------------
 ;; state
 ;; ----------------------------
-
 (def current-slide (r/atom 0))
 (def timer-id (atom nil))
 (def next-slide (r/atom nil))
@@ -16,7 +15,6 @@
 ;; ----------------------------
 ;; slide data
 ;; ----------------------------
-
 (def slides
   [{:key :chameleon
     :img "img/Hero_Slide_1_chameleon.jpg"
@@ -45,10 +43,6 @@
 ;; ----------------------------
 ;; random helpers
 ;; ----------------------------
-
-;; (defn random-slide []
-;;   (rand-int (count slides)))
-
 (defn random-slide []
   (let [idx (rand-int (count slides))]
     (if (= idx @current-slide)
@@ -61,60 +55,9 @@
 ;; ----------------------------
 ;; 🎲 switch function
 ;; ----------------------------
-
-;; (defn go-to-slide! [idx dir]
-;;   (when-not @animating?
-;;     (reset! direction dir)
-;;     (reset! next-slide idx)
-;;     (reset! animating? true)
-;;     ;; after a while we finish the animation
-;;     (js/setTimeout
-;;      (fn []
-;;        (reset! current-slide idx)
-;;        (reset! next-slide nil)
-;;        (reset! animating? false))
-;;      700)))
-;; (defn go-to-slide! [idx dir]
-;;   (when (and (not @animating?)
-;;              (not= idx @current-slide)) ;; 💡
-;; (defn go-to-slide! [idx dir]
-;;   (when-not @animating?
-;; (defn go-to-slide! [idx dir]
-;;   (when (and (not @animating?)
-;;              (not= idx @current-slide))
-;;     (reset! direction dir)
-;;     (reset! next-slide idx)
-;;     ;; first put it in the starting position
-;;     (reset! animating? :prepare)
-;;     (js/requestAnimationFrame
-;;      (fn []
-;;        ;; now let's start the animation
-;;        (reset! animating? :run)))
-;;     ;; completion
-;;     ;; (js/setTimeout
-;;     ;;  (fn []
-;;     ;;    (reset! current-slide idx)
-;;     ;;    (reset! next-slide nil)
-;;     ;;    (reset! animating? false))
-;;     ;;  700)))
-;;     ;; (js/setTimeout
-;;     ;;   (fn []
-;;     ;;     (reset! current-slide idx)
-;;     ;;     (reset! next-slide nil)
-;;     ;;     (reset! animating? false))
-;;     ;;   700)))
-;;     (js/setTimeout
-;;       (fn []
-;;         (reset! current-slide idx)
-;;         (js/requestAnimationFrame
-;;           (fn []
-;;             (reset! next-slide nil)
-;;             (reset! animating? false))))
-;;       700)))
-
 (defn go-to-slide! [idx dir]
   (when (not= idx @current-slide)
-    (let [id (swap! anim-id inc)] ;; 💡 уникальный id
+    (let [id (swap! anim-id inc)] 
 
       (reset! direction dir)
       (reset! next-slide idx)
@@ -122,12 +65,12 @@
 
       (js/requestAnimationFrame
        (fn []
-         (when (= id @anim-id) ;; 💡 проверка
+         (when (= id @anim-id)
            (reset! animating? :run))))
 
       (js/setTimeout
        (fn []
-         (when (= id @anim-id) ;; 💡 главный фикс
+         (when (= id @anim-id)
            (reset! current-slide idx)
            (reset! next-slide nil)
            (reset! animating? false)))
@@ -136,30 +79,6 @@
 ;; ----------------------------
 ;; auto switch
 ;; ----------------------------
-
-;; (defn schedule-next! []
-;;   (when @timer-id
-;;     (js/clearTimeout @timer-id))
-
-;;   (reset! timer-id
-;;           (js/setTimeout
-;;            (fn []
-;;              (reset! current-slide (random-slide))
-;;              (schedule-next!))
-;;            (random-delay))))
-
-;; (defn schedule-next! []
-;;   (when @timer-id
-;;     (js/clearTimeout @timer-id))
-
-;;   (reset! timer-id
-;;           (js/setTimeout
-;;            (fn []
-;;              (go-to-slide! (random-slide) :right)
-;;              (schedule-next!))
-;;            (random-delay))))
-
-
 (defn schedule-next! []
   (when @timer-id
     (js/clearTimeout @timer-id))
@@ -174,7 +93,6 @@
 ;; ----------------------------
 ;; 🎬 SLIDER (main magic)
 ;; ----------------------------
-
 (defn slide-style [pos]
   {:transform
    (case pos
@@ -186,78 +104,6 @@
 ;; ----------------------------
 ;; hero background
 ;; ----------------------------
-
-;; (defn background-slider []
-;;   (let [{:keys [img]} (nth slides @current-slide)]
-;;     [:div
-;;      {:class "absolute inset-0 bg-cover bg-center transition-all duration-1700"
-;;       :style {:background-image (str "url('" img "')")}}]))
-
-  ;; (defn background-slider []
-  ;;   (let [curr (nth slides @current-slide)
-  ;;         next (when @next-slide (nth slides @next-slide))
-  ;;         dir  @direction]
-  ;;     [:div {:class "absolute inset-0 overflow-hidden"}
-  ;;     ;; current
-  ;;     [:div
-  ;;       {:class "absolute inset-0 bg-cover bg-center"
-  ;;       :style (merge
-  ;;               {:background-image (str "url('" (:img curr) "')")}
-  ;;               (if @animating?
-  ;;                 (slide-style (if (= dir :right) :left :right))
-  ;;                 (slide-style :current)))}]
-  ;;       ;; next
-  ;;       (when next
-  ;;       [:div
-  ;;         {:class "absolute inset-0 bg-cover bg-center"
-  ;;         :style (merge
-  ;;                 {:background-image (str "url('" (:img next) "')")}
-  ;;                 {:transform (if (= dir :right)
-  ;;                               "translateX(100%)"
-  ;;                               "translateX(-100%)")
-  ;;                   :transition "transform 700ms ease-in-out"}
-  ;;         ;; let's start the movement
-  ;;                 (when @animating?
-  ;;                   {:transform "translateX(0%)"}))}])]))
-
-;; (defn background-slider []
-;;   (let [curr (nth slides @current-slide)
-;;         next (when @next-slide (nth slides @next-slide))
-;;         dir  @direction
-;;         anim @animating?]
-;;     [:div {:class "absolute inset-0 overflow-hidden"}
-;;     ;; next
-;;      (when next
-;;        [:div
-;;         {:class "absolute inset-0 bg-cover bg-center"
-;;          :style (merge
-;;                  {:background-image (str "url('" (:img next) "')")}
-;;                  {:transition "transform 700ms ease-in-out"}
-;;                  (cond
-;;                    ;; starting position
-;;                    (= anim :prepare)
-;;                    {:transform (if (= dir :right)
-;;                                  "translateX(100%)"
-;;                                  "translateX(-100%)")}
-;;                    ;; movement
-;;                    (= anim :run)
-;;                    {:transform "translateX(0%)"}))}])
-;;     ;; current
-;;      [:div
-;;       {:class "absolute inset-0 bg-cover bg-center"
-;;        :style (merge
-;;                {:background-image (str "url('" (:img curr) "')")}
-;;                {:transition "transform 700ms ease-in-out"}
-;;                (cond
-;;                  (= anim :run)
-;;                  {:transform (if (= dir :right)
-;;                                "translateX(-100%)"
-;;                                "translateX(100%)")}
-;;                  :else
-;;                  {:transform "translateX(0%)"}))}]
-;;      ]))
-
-
 (defn background-slider []
   (let [curr (nth slides @current-slide)
         next-idx (or @next-slide @current-slide)
@@ -300,7 +146,6 @@
 ;; ----------------------------
 ;; hero content
 ;; ----------------------------
-
 (defn hero-content []
   (let [{:keys [title text btn]} (nth slides @current-slide)]
     [:div
@@ -319,7 +164,6 @@
 ;; ----------------------------
 ;; mini navigation
 ;; ----------------------------
-
 (defn nav-item [idx {:keys [img]}]
   [:div
    {:class (str
@@ -330,10 +174,6 @@
 
     :style {:background-image (str "url('" img "')")}
 
-    ;; :on-mouse-enter
-    ;; (fn []
-    ;;   (reset! current-slide idx)
-    ;;   (schedule-next!))}])
     :on-mouse-enter
       (fn []
         (go-to-slide! idx :right)
@@ -343,15 +183,12 @@
 (defn mini-logo-navigation []
   [:div
    {:class "absolute bottom-6 right-6 grid grid-cols-2 gap-2 "}
-
-
    (doall
     (map-indexed nav-item slides))])
 
 ;; ----------------------------
 ;; main hero
 ;; ----------------------------
-
 (defn hero []
   (r/create-class
    {:component-did-mount
