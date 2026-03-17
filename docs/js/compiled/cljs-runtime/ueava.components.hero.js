a8f719c37d2692b3d213e7a7be3e74fb7add2dc1
buyn <buyn.tex@gmail.com>
2026-03-17 09:02:06 +0200

    pablish ions fy fix

--- docs/js/compiled/cljs-runtime/ueava.components.hero.js
+++ docs/js/compiled/cljs-runtime/ueava.components.hero.js
@@ -21,31 +21,31 @@ return (ueava.components.hero.schedule_next_BANG_.cljs$core$IFn$_invoke$arity$0 
 }),ueava.components.hero.random_delay()));
 });
 ueava.components.hero.background_slider = (function ueava$components$hero$background_slider(){
-var map__14453 = cljs.core.nth.cljs$core$IFn$_invoke$arity$2(ueava.components.hero.slides,cljs.core.deref(ueava.components.hero.current_slide));
-var map__14453__$1 = cljs.core.__destructure_map(map__14453);
-var img = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__14453__$1,new cljs.core.Keyword(null,"img","img",1442687358));
+var map__14511 = cljs.core.nth.cljs$core$IFn$_invoke$arity$2(ueava.components.hero.slides,cljs.core.deref(ueava.components.hero.current_slide));
+var map__14511__$1 = cljs.core.__destructure_map(map__14511);
+var img = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__14511__$1,new cljs.core.Keyword(null,"img","img",1442687358));
 return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"class","class",-2030961996),"absolute inset-0 bg-cover bg-center transition-all duration-1700",new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"background-image","background-image",-1142314704),["url('",cljs.core.str.cljs$core$IFn$_invoke$arity$1(img),"')"].join('')], null)], null)], null);
 });
 ueava.components.hero.hero_content = (function ueava$components$hero$hero_content(){
-var map__14454 = cljs.core.nth.cljs$core$IFn$_invoke$arity$2(ueava.components.hero.slides,cljs.core.deref(ueava.components.hero.current_slide));
-var map__14454__$1 = cljs.core.__destructure_map(map__14454);
-var title = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__14454__$1,new cljs.core.Keyword(null,"title","title",636505583));
-var text = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__14454__$1,new cljs.core.Keyword(null,"text","text",-1790561697));
-var btn = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__14454__$1,new cljs.core.Keyword(null,"btn","btn",1978294651));
+var map__14512 = cljs.core.nth.cljs$core$IFn$_invoke$arity$2(ueava.components.hero.slides,cljs.core.deref(ueava.components.hero.current_slide));
+var map__14512__$1 = cljs.core.__destructure_map(map__14512);
+var title = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__14512__$1,new cljs.core.Keyword(null,"title","title",636505583));
+var text = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__14512__$1,new cljs.core.Keyword(null,"text","text",-1790561697));
+var btn = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__14512__$1,new cljs.core.Keyword(null,"btn","btn",1978294651));
 return new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"relative z-10 text-white text-center max-w-2xl"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"text-5xl font-bold mb-4"], null),title], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p","p",151049309),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"text-xl mb-6"], null),text], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"button","button",1456579943),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"bg-white text-black px-6 py-3 rounded-lg"], null),btn], null)], null);
 });
-ueava.components.hero.nav_item = (function ueava$components$hero$nav_item(idx,p__14455){
-var map__14456 = p__14455;
-var map__14456__$1 = cljs.core.__destructure_map(map__14456);
-var img = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__14456__$1,new cljs.core.Keyword(null,"img","img",1442687358));
-return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"class","class",-2030961996),["w-16 h-16 bg-cover bg-center cursor-pointer border ",((cljs.core._EQ_.cljs$core$IFn$_invoke$arity$2(idx,cljs.core.deref(ueava.components.hero.current_slide)))?"border-white":null)].join(''),new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"background-image","background-image",-1142314704),["url('",cljs.core.str.cljs$core$IFn$_invoke$arity$1(img),"')"].join('')], null),new cljs.core.Keyword(null,"on-mouse-enter","on-mouse-enter",-1664921661),(function (){
+ueava.components.hero.nav_item = (function ueava$components$hero$nav_item(idx,p__14513){
+var map__14514 = p__14513;
+var map__14514__$1 = cljs.core.__destructure_map(map__14514);
+var img = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__14514__$1,new cljs.core.Keyword(null,"img","img",1442687358));
+return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"class","class",-2030961996),["w-16 h-16 bg-cover bg-center cursor-pointer border ",((cljs.core._EQ_.cljs$core$IFn$_invoke$arity$2(idx,cljs.core.deref(ueava.components.hero.current_slide)))?"border-white scale-110":"border-transparent opacity-70")].join(''),new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"background-image","background-image",-1142314704),["url('",cljs.core.str.cljs$core$IFn$_invoke$arity$1(img),"')"].join('')], null),new cljs.core.Keyword(null,"on-mouse-enter","on-mouse-enter",-1664921661),(function (){
 cljs.core.reset_BANG_(ueava.components.hero.current_slide,idx);
 
 return ueava.components.hero.schedule_next_BANG_();
 })], null)], null);
 });
 ueava.components.hero.mini_logo_navigation = (function ueava$components$hero$mini_logo_navigation(){
-return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"absolute bottom-6 right-6 grid grid-cols-2 gap-2"], null),cljs.core.doall.cljs$core$IFn$_invoke$arity$1(cljs.core.map_indexed.cljs$core$IFn$_invoke$arity$2(ueava.components.hero.nav_item,ueava.components.hero.slides))], null);
+return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"absolute bottom-6 right-6 grid grid-cols-2 gap-2 "], null),cljs.core.doall.cljs$core$IFn$_invoke$arity$1(cljs.core.map_indexed.cljs$core$IFn$_invoke$arity$2(ueava.components.hero.nav_item,ueava.components.hero.slides))], null);
 });
 ueava.components.hero.hero = (function ueava$components$hero$hero(){
 return reagent.core.create_class(new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"component-did-mount","component-did-mount",-1126910518),(function (){
